593af3b24a8663c3ac44afea2e000f39bcb887c9
Hernan <you@example.com>
2021-11-09 15:45:49 -0500

    version 1.0.0 apli pedidos

--- src/models/pedido.model.ts
+++ src/models/pedido.model.ts
@@ -1,5 +1,6 @@
-import {Entity, model, property, belongsTo} from '@loopback/repository';
+import {Entity, model, property, belongsTo, hasOne} from '@loopback/repository';
 import {Persona} from './persona.model';
+import {Producto} from './producto.model';
 
 @model()
 export class Pedido extends Entity {
@@ -14,7 +15,7 @@ export class Pedido extends Entity {
     type: 'string',
     required: true,
   })
-  producto: string;
+  id_producto: string;
 
   @property({
     type: 'number',
@@ -36,6 +37,9 @@ export class Pedido extends Entity {
 
   @belongsTo(() => Persona)
   personaId: string;
+
+  @hasOne(() => Producto)
+  producto: Producto;
 
   constructor(data?: Partial<Pedido>) {
     super(data);
